bf6261bb299d2a78e9a6ede4a405d9db30a46d0e
Yury Nechaev <yury.nechaev@team.wrike.com>
2015-01-20 16:53:44 +0300

    Initial Commit

---
+++ Hive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hive
+//
+//  Created by Yury Nechaev on 20.01.15.
+//  Copyright (c) 2015 Yury Nechaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hive
+//
+//  Created by Yury Nechaev on 20.01.15.
+//  Copyright (c) 2015 Yury Nechaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
